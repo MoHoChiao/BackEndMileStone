fbbba685280707a2512a5367474a3da9ccfc061d
User <User@DESKTOP-15MK05L>
2017-08-23 23:30:19 +0800

    update

---
+++ TrinityServiceUtil/src/main/java/com/netpro/trinity/service/util/entity/dto/App.java
@@ -0,0 +1,49 @@
+package com.netpro.trinity.service.util.entity.dto;
+
+import java.util.List;
+//宣告一個POJO
+public class App {
+	private String name;
+    private String cls;
+    private String img;
+    private String alt;
+    private String model;
+    private List<String> desc;
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCls() {
+		return cls;
+	}
+	public void setCls(String cls) {
+		this.cls = cls;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getAlt() {
+		return alt;
+	}
+	public void setAlt(String alt) {
+		this.alt = alt;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public List<String> getDesc() {
+		return desc;
+	}
+	public void setDesc(List<String> desc) {
+		this.desc = desc;
+	}
+}
---
+++ TrinityServiceUtil/src/main/java/com/netpro/trinity/service/util/status/TrinityServiceStatusMsg.java
@@ -0,0 +1,11 @@
+package com.netpro.trinity.service.util.status;
+
+public class TrinityServiceStatusMsg {
+	public static String LOGIN_ERROR = "Login Error!";
+	public static String LOGIN_SUCCESS = "Login Success.";
+	public static String ACCOUNT_EMPTY = "User Account can not be empty!";
+	public static String PSW_EMPTY = "User PSW can not be empty!";
+	public static String PSW_NEAR_EXPIRE = "Password is near expiration date!";
+	public static String ACCOUNT_CHECK = "Please Check Your Account!";
+	public static String RESET_PSW = "Please Reset your password!";
+}
