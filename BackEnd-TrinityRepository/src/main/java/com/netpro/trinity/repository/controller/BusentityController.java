df4e3a0911256d3bd0be318cfd55a28f42555380
hungw <hungweiliu543@gmail.com>
2017-11-17 17:44:31 +0800

    update

---
+++ BackEnd-TrinityRepository/src/main/java/com/netpro/trinity/repository/controller/BusentityController.java
@@ -0,0 +1,138 @@
+package com.netpro.trinity.repository.controller;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netpro.trinity.repository.dto.FilterInfo;
+import com.netpro.trinity.repository.jpa.entity.Busentity;
+import com.netpro.trinity.repository.service.BusentityService;
+
+@RestController  //宣告一個Restful Web Service的Resource
+@RequestMapping("/busentity")
+public class BusentityController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(BusentityController.class);
+		
+	@Autowired
+	private BusentityService service;
+	
+	@GetMapping("/findAll")
+	public ResponseEntity<?> findAllEntities() {
+		try {
+			return ResponseEntity.ok(this.service.getAll());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+	
+	@GetMapping("/isExistByUid")
+	public ResponseEntity<?> isEntityExistByUid(String uid) {
+		try {
+			return ResponseEntity.ok(this.service.existByUid(uid));
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+	
+	@GetMapping("/findByUid")
+	public ResponseEntity<?> findEntityByUid(String uid) {
+		try {
+			return ResponseEntity.ok(this.service.getByUid(uid));
+		}catch(IllegalArgumentException e) {
+			BusentityController.LOGGER.error("IllegalArgumentException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+  
+	@GetMapping("/findByName")
+	public ResponseEntity<?> findEntitiesByName(String name) {
+		try {
+			return ResponseEntity.ok(this.service.getByName(name));
+		}catch(IllegalArgumentException e) {
+			BusentityController.LOGGER.error("IllegalArgumentException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+	
+	@PostMapping("/findByFilter")
+	public ResponseEntity<?> findEntitiesByFilter(@RequestBody FilterInfo filter) {
+		try {
+			return this.service.getByFilter(filter);
+		}catch(SecurityException e) {
+			BusentityController.LOGGER.error("SecurityException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(NoSuchMethodException e) {
+			BusentityController.LOGGER.error("NoSuchMethodException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(IllegalAccessException e) {
+			BusentityController.LOGGER.error("IllegalAccessException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(InvocationTargetException e) {
+			BusentityController.LOGGER.error("InvocationTargetException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(IllegalArgumentException e) {
+			BusentityController.LOGGER.error("IllegalArgumentException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+  
+	@PostMapping("/add")
+	public ResponseEntity<?> addEntity(@RequestBody Busentity entity) {
+		try {
+			return ResponseEntity.ok(this.service.add(entity));
+		}catch(IllegalArgumentException e) {
+			BusentityController.LOGGER.error("IllegalArgumentException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+	
+	@PostMapping("/edit")
+	public ResponseEntity<?> editEntity(@RequestBody Busentity entity) {
+		try {
+			return ResponseEntity.ok(this.service.edit(entity));
+		}catch(IllegalArgumentException e) {
+			BusentityController.LOGGER.error("IllegalArgumentException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
+  
+	@GetMapping("/delete")
+	public ResponseEntity<?> deleteEntityByUid(String uid) {
+		try {
+			this.service.deleteByUid(uid);
+		}catch(IllegalArgumentException e) {
+			BusentityController.LOGGER.error("IllegalArgumentException; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}catch(Exception e) {
+			BusentityController.LOGGER.error("Exception; reason was:", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+		return ResponseEntity.ok(uid);
+	}
+}
